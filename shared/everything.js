da694559066f01631465c3c7e1b459b39baf54fb
huge-pancake <huge.pancake.zxr@gmail.com>
2022-08-17 08:38:33 +0800

    add `everything.js`

---
+++ shared/everything.js
@@ -0,0 +1,35 @@
+import Badge from "../badge/badge.js";
+import CommonButton from "../button/common-button.js";
+import FAB from "../button/fab.js";
+import IconButton from "../button/icon-button.js";
+import List from "../list/list.js";
+import ListItem from "../list/list-item";
+import Menu from "../menu/menu.js";
+import Popover from "../popover/popover";
+import Ripple from "../ripple/ripple.js";
+import TopAppBar from "../top-app-bar/top-app-bar.js";
+import Typography from "../typography/typography.js";
+
+import ActionElement from "./action-element.js";
+import ActionElementLabeled from "./action-element-labeled.js";
+import BaseElement from "./base-element.js";
+import Env from "./env.js";
+
+export {
+  Badge,
+  CommonButton,
+  FAB,
+  IconButton,
+  List,
+  ListItem,
+  Menu,
+  Popover,
+  Ripple,
+  TopAppBar,
+  Typography,
+
+  ActionElement,
+  ActionElementLabeled,
+  BaseElement,
+  Env,
+};
