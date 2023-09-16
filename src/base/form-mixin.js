bd2597034b3ff4b6372278dea60a66452e6482d5
vollowx <muffinslog@proton.me>
2023-09-16 16:37:09 +0800

    feat(`FormMixin`): extend form features

--- src/base/form-mixin.js
+++ src/base/form-mixin.js
@@ -11,13 +11,29 @@ const FormMixin = (Base) =>
     }
 
     get form() {
-      return this[internals].form ?? null;
+      return this[internals].form;
     }
     get name() {
       return this.getAttribute('name');
     }
     get type() {
       return this.localName;
+    }
+    get validity() {
+      return this[internals].validity;
+    }
+    get validationMessage() {
+      return this[internals].validationMessage;
+    }
+    get willValidate() {
+      return this[internals].willValidate;
+    }
+
+    checkValidity() {
+      return this[internals].checkValidity();
+    }
+    reportValidity() {
+      return this[internals].reportValidity();
     }
   };
 
