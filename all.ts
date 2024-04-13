67076aad240b0e8ac36391209a0ab50e9ee84ae9
vollowx <vollowx@proton.me>
2024-04-14 01:06:45 +0800

    feat(demo): add components

--- all.ts
+++ all.ts
@@ -1,4 +1,5 @@
 export { M3Button } from './src/m3/button.js';
+export { M3FAB } from './src/m3/fab.js';
 export { M3IconButton } from './src/m3/icon-button.js';
 export { M3IconButtonToggle } from './src/m3/icon-button-toggle.js';
 export { M3Tooltip } from './src/m3/tooltip.js';
