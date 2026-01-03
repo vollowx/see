9cb6d27693398e1a166b47043e8ca7704c8fa497
vollowx <vollowx@outlook.com>
2026-01-03 11:18:32 +0800

    feat(button): update structure

--- src/base/button.ts
+++ src/base/button.ts
@@ -8,56 +8,56 @@ import { hiddenStyles } from './hidden-styles.css.js';
 const Base = FormAssociated(InternalsAttached(LitElement));
 
 export class Button extends Base {
+  static override styles = [hiddenStyles];
+
+  @property({ reflect: true }) type: 'button' | 'submit' | 'reset' = 'button';
+  @property({ type: Boolean, reflect: true }) disabled = false;
+
   constructor() {
     super();
     this[internals].role = 'button';
     this.#updateInternals();
   }
-  static override styles = [hiddenStyles];
+
   override connectedCallback() {
     super.connectedCallback();
-    this.addEventListener('keydown', this.#boundKeyDown);
-    this.addEventListener('keyup', this.#boundKeyUp);
-    this.addEventListener('click', this.#boundClick);
+    this.addEventListener('keydown', this.#handleKeyDown);
+    this.addEventListener('keyup', this.#handleKeyUp);
+    this.addEventListener('click', this.#handleClick);
   }
+
   override disconnectedCallback() {
     super.disconnectedCallback();
-    this.removeEventListener('keydown', this.#boundKeyDown);
-    this.removeEventListener('keyup', this.#boundKeyUp);
-    this.removeEventListener('click', this.#boundClick);
+    this.removeEventListener('keydown', this.#handleKeyDown);
+    this.removeEventListener('keyup', this.#handleKeyUp);
+    this.removeEventListener('click', this.#handleClick);
   }
+
   protected override updated(changed: Map<string, any>) {
-    if (changed.has('disabled')) {
-      this.#updateInternals();
-    }
+    if (changed.has('disabled')) this.#updateInternals();
   }
-  @property({ reflect: true }) type: 'button' | 'submit' | 'reset' = 'button';
-  @property({ type: Boolean, reflect: true }) disabled = false;
 
   #updateInternals() {
-    this.setAttribute('tabindex', this.disabled ? '-1' : '0');
-    this[internals].ariaDisabled = this.disabled ? 'true' : 'false';
+    this.tabIndex = this.disabled ? -1 : 0;
+    this[internals].ariaDisabled = String(this.disabled);
   }
 
-  #boundKeyDown = this.#handleKeyDown.bind(this);
-  #boundKeyUp = this.#handleKeyUp.bind(this);
-  #boundClick = this.#handleClick.bind(this);
-  #handleKeyDown(e: KeyboardEvent) {
+  #handleKeyDown = (e: KeyboardEvent) => {
     if (e.key !== ' ' && e.key !== 'Enter') return;
     e.preventDefault();
     e.stopPropagation();
-    if (e.key === 'Enter') {
-      this.click();
-    }
-  }
-  #handleKeyUp(e: KeyboardEvent) {
+    if (e.key === 'Enter') this.click();
+  };
+
+  #handleKeyUp = (e: KeyboardEvent) => {
     if (e.key === ' ') {
       e.preventDefault();
       e.stopPropagation();
       this.click();
     }
-  }
-  #handleClick() {
+  };
+
+  #handleClick = () => {
     if (this.type !== 'button') this[internals].form?.[this.type]();
-  }
+  };
 }
